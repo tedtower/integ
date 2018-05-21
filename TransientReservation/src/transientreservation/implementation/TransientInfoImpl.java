69e277abeb195d0fbdbf1fb1c99dc0d259384c4c
tedtower <2168162@slu.edu.ph>
2018-05-21 14:11:27 +0800

    updated the implementation and interface classes

--- TransientReservation/src/transientreservation/implementation/TransientInfoImpl.java
+++ TransientReservation/src/transientreservation/implementation/TransientInfoImpl.java
@@ -4,10 +4,42 @@
  * and open the template in the editor.
  */
 package transientreservation.implementation;
+
+import java.rmi.RemoteException;
+import transientreservation.interfaces.TransientInfo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  *
  * @author HP
  */
-public class TransientInfoImpl {
+public class TransientInfoImpl implements TransientInfo{
+    private Connection con;
     
+    public TransientInfoImpl(){
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/transient_house","root","");
+        }catch(SQLException ex){
+            System.out.println("Encountered a problem connection to the database!");
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void setTransientName(String name) throws RemoteException {
+        
+    }
+
+    @Override
+    public void setRoomCapacity(int capacity) throws RemoteException {
+        
+    }
+
+    @Override
+    public void sendNotification(String notif) throws RemoteException {
+        
+    }
 }
--- TransientReservation/src/transientreservation/interfaces/TransientInfo.java
+++ TransientReservation/src/transientreservation/interfaces/TransientInfo.java
@@ -6,12 +6,13 @@ package transientreservation.interfaces;
  */
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 /**
  *
  * @author Ramos, Tatum Eiffel Dodge
  */
 public interface TransientInfo extends Remote{
     public void setTransientName(String name) throws RemoteException;
-    public void setCapacity(int capacity) throws RemoteException;
+    public void setRoomCapacity(int capacity) throws RemoteException, SQLException;
     public void sendNotification(String notif) throws RemoteException;
 }
